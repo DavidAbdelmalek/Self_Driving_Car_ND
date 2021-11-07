edd25100499792479a777fc914c4bc1be9d20c2f
David Abdelmalek <davidonsy123@gmail.com>
2021-11-07 11:50:46 +0100

    Update EDA.py

--- nd013-c1-vision-starter/EDA.py
+++ nd013-c1-vision-starter/EDA.py
@@ -2,6 +2,8 @@ import os
 import glob
 from utils import get_dataset
 import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
 
 if __name__ == "__main__":
     # Dictionary of number of classes in each image.
@@ -9,31 +11,49 @@ if __name__ == "__main__":
     records = []
     base_dir = 'data/'
     
+    # Average brightness per image
     brightness = []
-    dic_classes = {}
+    
+    # Label mapping
+    label_map = {1:'vehicle',
+                 2:'pedestrian',
+                 4:'cyclist'}
+    
+    dic_classes = {'vehicle':0,'pedestrian':0,'cyclist':0}
+    num_obj_img = []
     
     for target in directories:
         records = get_dataset(base_dir + f'{target}*')
         classes_images = []
+        i = 0 
         for batch in records:
             img = batch['image'].numpy()
             brightness.append(np.mean(img))
 
             classes_images.append(len(batch['groundtruth_classes'].numpy()))
-            for class_ in batch['groundtruth_classes'].numpy():
-                if not class_ in dic_classes:
-                    dic_classes[class_]=1
-                else:
-                    dic_classes[class_]+=1
-               
+            ground_truth_classes = batch['groundtruth_classes'].numpy()
+            for class_ in ground_truth_classes:
+                class_ = label_map[class_]
+                dic_classes[class_]+=1
             
-    print(dic_classes)
-    print(classes_image)
-    #recordss)
-    #classes_images = []
-    #i = 0 
-
-    #    print(batch)
-    #plt.hist(classes_images, bins=30)  # density=False would make counts
-    #plt.ylabel('Probability')
-    #plt.xlabel('Data');
+            num_obj_img.append(len(ground_truth_classes))
+          
+        
+    # Subplots
+    f, ax = plt.subplots(1,3, figsize=(12, 4))
+    ax[0].hist(brightness, density=True)
+    ax[0].set_title("Brightness per image")
+    
+    
+    keys = dic_classes.keys()
+    values = dic_classes.values()
+    obj_class_dist = np.array(list(Counter(values)))
+    obj_class_dist = obj_class_dist/float(obj_class_dist.sum())
+    ax[1].bar(keys, obj_class_dist)
+    ax[1].set_title("Object class distribution")
+    
+    ax[2].hist(num_obj_img, density=True)
+    ax[2].set_title("Number of objects per image")
+    
+    plt.savefig("images/EDA.png")
+    
